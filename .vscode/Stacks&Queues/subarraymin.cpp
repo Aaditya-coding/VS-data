04fce146a61f603a533afa7e4dfb0aedfbd6efce
Aaditya-coding <aaditya200614@gmail.com>
2026-08-02 19:10:29 +0530

    2nd August,2026

---
+++ .vscode/Stacks&Queues/subarraymin.cpp
@@ -0,0 +1,9 @@
+// To identify how much times a particular element will be smallest in its subarray, just find the difference between the index of its own and nse and add it to the difference between the index of its own and the pse. 
+#include<bits/stdc++.h>
+using namespace std;
+// Time Complexity = 
+// Space Complexity = 
+class Solution{
+    
+};
+
